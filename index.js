02bb9ffd724b21a88c3f897dd1514d37b16d32a7
Patrick Dunn <pmdunn78@gmail.com>
2021-04-18 10:34:02 -0500

    wrap manual array in function

--- index.js
+++ index.js
@@ -4,64 +4,53 @@ var Engineer = require('./lib/Engineer');
 var Intern = require('./lib/Intern');
 var fs = require('fs');
 
+let team = [];
 
 
 
-const exampleArray = [];
-var el_Lic = new Manager('Luis', 1, 'el_lic@mandamanda.com', 1)
+const exampleBuild = () => {
+    let exampleArray = [];
+    var el_Lic = new Manager('Luis', 1, 'el_lic@mandamanda.com', 1)
 
-var Simone = new Engineer('Simone', 2, 'simone@gmail.com', 'knightmac19');
-var Maria = new Engineer('Maria', 3, 'maria@gmail.com', 'knightmac19');
-var Jennifer = new Engineer('Jennifer', 4, 'jenn@gmail.com', 'knightmac19');
-var Victoria = new Engineer('Victoria', 11, 'victoria@gmail.com', 'knightmac19');
-var Patrick = new Engineer('Patrick', 10, 'patrick@gmail.com', 'knightmac19');
-var Robert = new Engineer('Robert', 9, 'rob@gmail.com', 'knightmac19');
+    // var Simone = new Engineer('Simone', 2, 'simone@gmail.com', 'knightmac19');
+    // var Maria = new Engineer('Maria', 3, 'maria@gmail.com', 'knightmac19');
+    // var Jennifer = new Engineer('Jennifer', 4, 'jenn@gmail.com', 'knightmac19');
+    var Victoria = new Engineer('Victoria', 11, 'victoria@gmail.com', 'knightmac19');
+    var Patrick = new Engineer('Patrick', 10, 'patrick@gmail.com', 'knightmac19');
+    var Robert = new Engineer('Robert', 9, 'rob@gmail.com', 'knightmac19');
 
+    var Mark = new Intern('Mark', 5, 'mark@gmail.com', 'knightmac19');
+    var Chris = new Intern('Chris', 6, 'chris@gmail.com', 'knightmac19');
 
-var Mark = new Intern('Mark', 5, 'mark@gmail.com', 'knightmac19');
-var Chris = new Intern('Chris', 6, 'chris@gmail.com', 'knightmac19');
+    exampleArray.push(Victoria)
+    exampleArray.push(Patrick)
+    exampleArray.push(Robert)
 
-exampleArray.push(Victoria)
-exampleArray.push(Patrick)
-exampleArray.push(Robert)
+    // exampleArray.push(Natalia)
+    // exampleArray.push(Mike)
+    // exampleArray.push(John)
+    exampleArray.push(el_Lic)
+    // exampleArray.push(Simone)
+    // exampleArray.push(Maria)
+    // exampleArray.push(Jennifer)
+    exampleArray.push(Mark)
+    exampleArray.push(Chris)
 
+    return exampleArray;
+}
 
-// exampleArray.push(Natalia)
-// exampleArray.push(Mike)
-// exampleArray.push(John)
-exampleArray.push(el_Lic)
-exampleArray.push(Simone)
-exampleArray.push(Maria)
-exampleArray.push(Jennifer)
-exampleArray.push(Mark)
-exampleArray.push(Chris)
-
-var engineers = exampleArray.filter(function(obj) {
-    return obj.github;
-});
-
-var managers = exampleArray.filter(function(obj) {
-    return obj.officeNumber;
-});
-
-var interns = exampleArray.filter(function(obj) {
-    return obj.school;
-});
-
-// console.log(exampleArray)
-// console.log(managers)
-
-
-
-    fs.writeFile('./dist/index.html', generate(exampleArray), function(err) {
+const buildFinalFile = arr => {
+    fs.writeFile('./dist/index.html', generate(arr), function(err) {
         if (err) {
             console.log(err);
         }
         console.log('please open dist/ folder')
     });
+}
 
-
-// buildFinalFile(exampleArray)
+// team = exampleBuild();
+    
+buildFinalFile(team)
 
 
 
