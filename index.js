c90d5cca7ea3079ae434bbd87c15c24dc766abad
Patrick Dunn <pmdunn78@gmail.com>
2021-04-17 23:12:01 -0500

    build example array

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const generate = require('./templates/generateHtml');
+var generate = require('./templates/generateHtml');
+var Manager = require('./lib/Manager');
+var Engineer = require('./lib/Engineer');
+var Intern = require('./lib/Intern');
 var fs = require('fs');
 
 
-fs.writeFile('./dist/index.html', generate, function(err) {
-    if (err) {
-        console.log(err);
-    }
-    console.log('please open dist/ folder')
-});
+
+
+const exampleArray = [];
+var John = new Manager('John', 1, 'johnny@appleseed.com', 2);
+var Simone = new Engineer('Simone', 2, 'simone@gmail.com', 'knightmac19');
+var Maria = new Engineer('Maria', 3, 'maria@gmail.com', 'knightmac19');
+var Jennifer = new Engineer('Jennifer', 4, 'jenn@gmail.com', 'knightmac19');
+var Mark = new Intern('Mark', 5, 'mark@gmail.com', 'knightmac19');
+var Chris = new Intern('Chris', 6, 'chris@gmail.com', 'knightmac19');
+
+exampleArray.push(John)
+exampleArray.push(Simone)
+exampleArray.push(Maria)
+exampleArray.push(Jennifer)
+exampleArray.push(Mark)
+exampleArray.push(Chris)
+
+
+
+console.log(exampleArray)
+
+
+const buildFinalFile = () => {
+    fs.writeFile('./dist/index.html', generate, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log('please open dist/ folder')
+    });
+}
+
+
 
 
